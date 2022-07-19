2ce205a7aab97c5c3def502bfb6a013dc93b72ee
tandica <williams.tandica@gmail.com>
2022-07-19 19:40:36 -0400

    add valid anagram problem

---
+++ arrays+hashing/leetcode_242.js
@@ -0,0 +1,18 @@
+const s = "rat";
+const t = "car";
+
+var isAnagram = function (s, t) {
+  //the split method turns the string into an array which then can be sorted and joined together back into a string
+  //the string ends up being sorted alphabetically
+  const sortedFirstString = s.split("").sort().join();
+  const sortedSecondString = t.split("").sort().join();
+
+  //compare if the sorted result is identical
+  if (sortedFirstString === sortedSecondString) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+isAnagram(s, t);
